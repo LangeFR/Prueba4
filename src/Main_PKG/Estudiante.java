c65216de80d9b561ad7d5a60bbe6d2acdcd3bd4f
Mateo Vanegas <mdjvanegasc@gmail.com>
2023-11-17 11:33:44 -0500

    Constructor Estudiante

--- src/Main_PKG/Estudiante.java
+++ src/Main_PKG/Estudiante.java
@@ -1,6 +1,11 @@
 package Main_PKG;
 
 public class Estudiante{
-    
+    String nombre;
+    String id;
+    Estudiante(String nombre, String id){
+        this.nombre = nombre;
+        this.id = id;
+    }
 
 }
